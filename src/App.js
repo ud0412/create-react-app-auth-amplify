78d66d74cd6986ba785468308c5f2397a7b09ddb
Sukmin Choi <ud0412@gmail.com>
2020-07-04 18:56:45 +0900

    material ui test 1

--- src/App.js
+++ src/App.js
@@ -5,25 +5,13 @@ import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
+import  {Button} from '@material-ui/core'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Button color='primary'>Show the answer</Button>
       </div>
     );
   }
